6b393c06d997f66afe045b1c6eed25afd301dc61
SumitGA <sghost33@gmail.com>
2026-06-28 04:46:20 +0000

    Terraform connected to Openstack: network + subnet defined

--- main.tf
+++ main.tf
@@ -27,3 +27,17 @@ data "openstack_networking_network_v2" "external" {
 output "external_network_id" {
   value = data.openstack_networking_network_v2.external.id
 }
+
+# A NEW network, defined in code. Named tf-* 
+resource "openstack_networking_network_v2" "tf_net" {
+  name           = "tf-net"
+  admin_state_up = true
+}
+
+resource "openstack_networking_subnet_v2" "tf_subnet" {
+  name            = "tf-subnet"
+  network_id      = openstack_networking_network_v2.tf_net.id
+  cidr            = "10.10.0.0/24"
+  ip_version      = 4
+  dns_nameservers = ["1.1.1.1"]
+}
